58235ec8af043468d473f1b2108a1954e151031f
Matt <towski@gmail.com>
2008-08-04 23:51:54 -0700

    Fixing

--- app/controllers/mirrors_controller.rb
+++ app/controllers/mirrors_controller.rb
@@ -1,4 +1,4 @@
-class MirrorController < ApplicationController
+class MirrorsController < ApplicationController
   def update
     Mirror.find_or_create_by_key(params[:mirror].delete(:key), params[:mirror])
     render :nothing => true
--- spec/controllers/mirrors_controller_spec.rb
+++ spec/controllers/mirrors_controller_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe MirrorController do
+describe MirrorsController do
 
   it "updates with a url and key" do
     put :update, :mirror => {:url => "http://bymatthew.com", :key => 1}
