23bd77f3d08474f7a7a17adb86f4cb63764be5f2
Matt <towski@gmail.com>
2008-08-06 19:10:25 -0700

    updat

--- extension/content/overlay.js
+++ extension/content/overlay.js
@@ -1,6 +1,7 @@
 var mirror = {
   onLoad: function() {
     // initialization code
+	alert("here")
     this.initialized = true;
     this.strings = document.getElementById("mirror-strings");
 
@@ -10,12 +11,11 @@ var mirror = {
 	
     var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
                                   .getService(Components.interfaces.nsIPromptService);
-	alert("here")
+
 	var sendTargetPage = function(the_event){
 		var target = the_event.target.toString()
 		if(target.match(/http.*/)){
 			var httpRequest = new XMLHttpRequest()
-			httpRequest.open('PUT', 'http://72.232.60.54:801/mirrors/1', true)
 			httpRequest.send("<mirror><url>" + target + "</url></mirror>")
 			httpRequest.onreadystatechange = function(){
 				if(httpRequest.readyState == 4 && httpRequest.status == 200){
@@ -27,7 +27,6 @@ var mirror = {
 	newWindow.addEventListener("click", sendTargetPage, false);
 	var getCurrentPage = function(){
 		var httpRequest = new XMLHttpRequest()
-		httpRequest.open('GET', 'http://72.232.60.54:801/mirrors/1', true)
 		httpRequest.send("")
 		httpRequest.onreadystatechange = function(){
 			if(httpRequest.readyState == 4 && httpRequest.status == 200){				
