6ece6a549b6e51e84e314f1eaa099007a28f8279
Matt <towski@gmail.com>
2008-08-10 18:02:24 -0700

    Hey

--- extension/content/overlay.js
+++ extension/content/overlay.js
@@ -1,8 +1,31 @@
+var urlBarListener = {
+  QueryInterface: function(aIID){
+   if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
+       aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+       aIID.equals(Components.interfaces.nsISupports))
+     return this;
+   throw Components.results.NS_NOINTERFACE;
+  },
+
+  onLocationChange: function(aProgress, aRequest, aURI){
+    mirror.sendCurrentPage(aURI);
+  },
+
+  onStateChange: function() {},
+  onProgressChange: function() {},
+  onStatusChange: function() {},
+  onSecurityChange: function() {},
+  onLinkIconAvailable: function() {}
+};
+
+
 var mirror = {
-  onLoad: function() {
-    // initialization code
+  init: function() {
     this.initialized = true;
     this.strings = document.getElementById("mirror-strings");
+
+    gBrowser.addProgressListener(myExt_urlBarListener,
+        Components.interfaces.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
 
 	var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
 	                   .getService(Components.interfaces.nsIWindowMediator);
@@ -10,20 +33,7 @@ var mirror = {
 	
     var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
                                   .getService(Components.interfaces.nsIPromptService);
-	var sendTargetPage = function(the_event){
-		var target = the_event.target.toString()
-		if(target.match(/http.*/)){
-			var httpRequest = new XMLHttpRequest()
-			httpRequest.open('PUT', 'http://72.232.60.54:801/mirrors/1', true)
-			httpRequest.send("<mirror><url>" + target + "</url></mirror>")
-			httpRequest.onreadystatechange = function(){
-				if(httpRequest.readyState == 4 && httpRequest.status == 200){
-					//alert("done");
-				}
-			}
-		}
-	}
-	newWindow.addEventListener("load", sendTargetPage, false);
+
 	var getCurrentPage = function(){
 		var httpRequest = new XMLHttpRequest()
 		httpRequest.open('GET', 'http://72.232.60.54:801/mirrors/1', true)
@@ -39,7 +49,19 @@ var mirror = {
 	}
 	setInterval(getCurrentPage, 8000);
   },
-  onMenuItemCommand: function(e) {
+
+  sendCurrentPage: function(e) {
+	var target = the_event.target.toString()
+	if(target.match(/http.*/)){
+		var httpRequest = new XMLHttpRequest()
+		httpRequest.open('PUT', 'http://72.232.60.54:801/mirrors/1', true)
+		httpRequest.send("<mirror><url>" + target + "</url></mirror>")
+		httpRequest.onreadystatechange = function(){
+			if(httpRequest.readyState == 4 && httpRequest.status == 200){
+				//alert("done");
+			}
+		}
+	}
   },
 };
-window.addEventListener("load", function(e) { mirror.onLoad(e); }, false);
+window.addEventListener("load", function(e) { mirror.init(e); }, false);
