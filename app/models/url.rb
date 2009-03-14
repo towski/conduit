f3c776e3ea1c3aab6a214e04a2908c40789a2842
towski <towski@gmail.com>
2009-03-14 14:30:01 -0700

    tweaking

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,8 @@ require 'digest/sha1'
 
 class Url < ActiveRecord::Base
   belongs_to :user
+  
+  def to_s
+    url
+  end
 end
--- extension/content/overlay.js
+++ extension/content/overlay.js
@@ -1,101 +1,95 @@
 //var railsServer = 'http://72.232.60.54'
-var railsServer = 'http://localhost:3000'
-var urlBarListener = {
-  QueryInterface: function(aIID){
-   if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
-       aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
-       aIID.equals(Components.interfaces.nsISupports))
-     return this
-   throw Components.results.NS_NOINTERFACE
-  },
-
-  onLocationChange: function(aProgress, aRequest, aURI){
-	conduit.sending = true
-    conduit.sendCurrentPage(aURI)
-  },
-
-  onStateChange: function() {},
-  onProgressChange: function() {},
-  onStatusChange: function() {},
-  onSecurityChange: function() {},
-  onLinkIconAvailable: function() {}
-}
-
-var conduit = {
-  oldURL: null,
-  key: null,
-  pid: null,
-  tab: null,
-  sending: false,
-  
-  init: function() {
-    this.initialized = true
-    this.strings = document.getElementById("mirror-strings")
-	gBrowser.removeProgressListener(urlBarListener)
-  },
-
-  getCurrentPage: function(){
-	if(conduit.sending == true){
-	  return
-	}
-  	var httpRequest = new XMLHttpRequest()
-  	httpRequest.open('GET', railsServer + '/conduits/'+conduit.key+'.json', true)
-  	httpRequest.send("")
-  	httpRequest.onreadystatechange = function(){
-  		if(httpRequest.readyState == 4 && httpRequest.status == 200){
-			if(conduit.sending == false){				
-	  			if(gBrowser.getBrowserForTab(conduit.tab).contentDocument.location.toString() != httpRequest.responseText){
-				    gBrowser.getBrowserForTab(conduit.tab).contentDocument.location = httpRequest.responseText
-	  			}
-			}
-  		}
-  	}
-  },
-
-  sendCurrentPage: function(aURI) {  
-	if (aURI.spec == this.oldURL){
-	  this.sending = false
-      return
-	}
-    this.oldURL = aURI.spec
-	if(gBrowser.selectedTab == this.tab && aURI.spec.match(/http.*/)){
-		var httpRequest = new XMLHttpRequest()
-		httpRequest.open('POST', railsServer + '/users/'+this.key+'/urls/', true)
-		httpRequest.send("<url><url>" + aURI.spec + "</url></url>")
-		httpRequest.onreadystatechange = function(){
-			if(httpRequest.readyState == 4 && httpRequest.status == 200){
-				conduit.sending = false
-			}
-		}
-	}
-  },
-
-  enable: function(key){
-	if(this.key){
-		alert("Sorry, only one conduit at a time (for the time being)")
-		return
-	}
-		this.key = key
-		gBrowser.addProgressListener(urlBarListener, Components.interfaces.nsIWebProgress.NOTIFY_STATE_DOCUMENT)
-		//this.pid = setInterval(this.getCurrentPage, 2000)
-		var initialRequest = new XMLHttpRequest()
-	  	initialRequest.open('GET', railsServer + '/conduits/'+this.key+'.json', true)
-	  	initialRequest.send("")		
-	  	initialRequest.onreadystatechange = function(){
-	  		if(initialRequest.readyState == 4 && initialRequest.status == 200){
-				conduit.tab = gBrowser.addTab(initialRequest.responseText)
-				gBrowser.selectedTab = conduit.tab
-				conduit.tab.addEventListener("TabClose", function(){ conduit.disable() }, false)
-			}
-		}
-  },
-
-  disable: function(){
-	this.key = null
-	gBrowser.removeProgressListener(urlBarListener)
-	//clearInterval(this.pid)
-  }
-}
-window.addEventListener("load", function(e) { conduit.init(e) }, false)
-document.addEventListener("activateConduit", function(e) { conduit.enable(e.target.getAttribute("conduit")) }, false, true)
-//document.addEventListener("showConduits", function(e) { e.target.setAttribute("style", 'display: block;') }, false, true)
+alert("Here");	// 
+// var railsServer = 'http://localhost:3000'
+// var urlBarListener = {
+//   QueryInterface: function(aIID){
+//    if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
+//        aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+//        aIID.equals(Components.interfaces.nsISupports))
+//      return this
+//    throw Components.results.NS_NOINTERFACE
+//   },
+// 
+//   onLocationChange: function(aProgress, aRequest, aURI){
+//     conduit.sendCurrentPage(aURI)
+//   },
+// 
+//   onStateChange: function() {},
+//   onProgressChange: function() {},
+//   onStatusChange: function() {},
+//   onSecurityChange: function() {},
+//   onLinkIconAvailable: function() {}
+// }
+// 
+// var conduit = {
+//   oldURL: null,
+//   key: null,
+//   pid: null,
+//   tab: null,
+//   sending: false,
+//   
+//   init: function() {
+//     this.initialized = true
+//     this.strings = document.getElementById("mirror-strings")
+// 		gBrowser.removeProgressListener(urlBarListener)
+//   },
+// 
+//   getCurrentPage: function(){
+// 	if(conduit.sending == true){
+// 	  return
+// 	}
+//   	var httpRequest = new XMLHttpRequest()
+//   	httpRequest.open('GET', railsServer + '/conduits/'+conduit.key+'.json', true)
+//   	httpRequest.send("")
+//   	httpRequest.onreadystatechange = function(){
+//   		if(httpRequest.readyState == 4 && httpRequest.status == 200){
+// 			if(conduit.sending == false){				
+// 	  			if(gBrowser.getBrowserForTab(conduit.tab).contentDocument.location.toString() != httpRequest.responseText){
+// 				    gBrowser.getBrowserForTab(conduit.tab).contentDocument.location = httpRequest.responseText
+// 	  			}
+// 			}
+//   		}
+//   	}
+//   },
+// 
+//   sendCurrentPage: function(aURI) {  
+// 		if (aURI.spec == this.oldURL){
+// 		   return
+// 		}
+// 		this.oldURL = aURI.spec
+// 		if(gBrowser.selectedTab == this.tab && aURI.spec.match(/http.*/)){
+// 			var httpRequest = new XMLHttpRequest()
+// 			httpRequest.open('POST', railsServer + '/users/1/urls/', true)
+// 			httpRequest.send("<url><url>" + aURI.spec + "</url></url>")
+// 			httpRequest.onreadystatechange = function(){
+// 				if(httpRequest.readyState == 4 && httpRequest.status == 200){
+// 					conduit.sending = false
+// 				}
+// 			}
+// 		}
+//   },
+// 
+//   enable: function(key){
+//     gBrowser.addProgressListener(urlBarListener, Components.interfaces.nsIWebProgress.NOTIFY_STATE_DOCUMENT)
+//     //this.pid = setInterval(this.getCurrentPage, 2000)
+//     //var initialRequest = new XMLHttpRequest()
+//     //initialRequest.open('GET', railsServer + '/conduits/'+this.key+'.json', true)
+//     //initialRequest.send("")		
+//     //initialRequest.onreadystatechange = function(){
+//     	//if(initialRequest.readyState == 4 && initialRequest.status == 200){
+//     conduit.tab = gBrowser.addTab("ey")//initialRequest.responseText)
+//     gBrowser.selectedTab = conduit.tab
+//     conduit.tab.addEventListener("TabClose", function(){ conduit.disable() }, false)
+//     	//}
+//     //}
+//   },
+// 
+//   disable: function(){
+// 		this.key = null
+// 		gBrowser.removeProgressListener(urlBarListener)
+// 		//clearInterval(this.pid)
+//   }
+// }
+// //window.addEventListener("load", function(e) { conduit.init(e) }, false)
+// //document.addEventListener("activateConduit", function(e) { conduit.enable(e.target.getAttribute("conduit")) }, false, true)
+// //document.addEventListener("showConduits", function(e) { e.target.setAttribute("style", 'display: block;') }, false, true)
